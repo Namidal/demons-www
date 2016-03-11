2c5cdb79c5fc7cccde9eccdd1ce234849e2699d2
Namidal <humainbanal@hotmail.fr>
2016-03-11 16:40:03 +0100

    Update config.php

--- Private/config.php
+++ Private/config.php
@@ -1,6 +1,6 @@
 <?php
 	try{
-		$db = new PDO('mysql:host=localhost;dbname=dmm;charset=utf8','dmm','p4snyjzVzmwW4Bhd');
+		$db = new PDO('mysql:host=mariadb;dbname=dmm;charset=utf8','dmm','p4snyjzVzmwW4Bhd');
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 	} catch(Exception $e) {
 		die('Erreur : '.$e->getMessage());
